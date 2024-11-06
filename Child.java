5e5d9ac1605fd18bbbdad0c89cc25e13fd42e1b0
yemyoungwoo <tkddlektks@gmail.com>
2024-11-06 10:26:45 +0900

    상속 클래스

---
+++ Child.java
@@ -0,0 +1,27 @@
+package test2;
+
+public class Child extends Parent {
+	
+	public Child() {
+	}
+	
+	public void subtract() {
+		int z = x - y;
+		System.out.printf("%d - %d = %d\n",x ,y ,z);
+	}
+	public static void main(String[] args) {
+		
+//		Parent t = new Parent();
+//		System.out.println("parents add");
+//		t.add();
+		
+		Child c = new Child();
+		System.out.println("child add");
+		c.add();
+		c.subtract();
+		
+	
+	
+	}
+
+}
---
+++ Parent.java
@@ -0,0 +1,32 @@
+package test2;
+
+public class Parent {
+	
+	int x;
+	int y;
+	
+	public Parent() {
+		x = (int)(Math.random()*100);
+		y = (int)(Math.random()*100);
+	}
+	
+	public void subtract() {
+		System.out.println("parent subtract");
+		int z = x - y;
+		System.out.printf("%d - %d = %d\n",x ,y ,z);
+	}
+	
+	public void add() {
+		
+		int z = x + y;
+	
+		System.out.printf("%d + %d = %d\n", x, y, z);
+	}
+	public static void main(String[] args) {
+		
+		Child c = new Child();
+		System.out.println("child add");
+		c.add();
+	
+	}
+}
